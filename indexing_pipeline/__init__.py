3891cdfa39d23676707d5cfaa856b7757ef03cd2
Samip Gupta <099samipgupta@gmail.com>
2026-09-08 08:45:45 +0530

    Fix indexing pipeline circular import

--- indexing_pipeline/__init__.py
+++ indexing_pipeline/__init__.py
@@ -1,15 +1,25 @@
 from indexing_pipeline.document import IndexedDocument
 from indexing_pipeline.indexer import DocumentIndexer
 from indexing_pipeline.crawler_bridge import CrawlerIndexBridge
-from indexing_pipeline.pipeline import CrawlIndexPipeline
 from indexing_pipeline.version_state import VersionState
 from indexing_pipeline.versioned_index import VersionedIndex
+
 
 __all__ = [
     "IndexedDocument",
     "DocumentIndexer",
     "CrawlerIndexBridge",
-    "CrawlIndexPipeline",
     "VersionState",
     "VersionedIndex",
+    "CrawlIndexPipeline",
 ]
+
+
+def __getattr__(name):
+    if name == "CrawlIndexPipeline":
+        from indexing_pipeline.pipeline import CrawlIndexPipeline
+        return CrawlIndexPipeline
+
+    raise AttributeError(
+        f"module {__name__!r} has no attribute {name!r}"
+    )
