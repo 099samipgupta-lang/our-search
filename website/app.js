6419ef0efceb798cdc0657608e099aaa7265fe8f
Samip Gupta <099samipgupta@gmail.com>
2026-09-09 17:24:10 +0530

    Fix search page navigation

--- website/app.js
+++ website/app.js
@@ -1,58 +1,6 @@
 (function () {
     "use strict";
 
-    function openSearchPage() {
-        fetch("/search.html")
-            .then(function (response) {
-                if (!response.ok) {
-                    throw new Error(
-                        "Search page returned HTTP " + response.status
-                    );
-                }
-
-                return response.text();
-            })
-            .then(function (html) {
-                var parser = new DOMParser();
-
-                var newDocument = parser.parseFromString(
-                    html,
-                    "text/html"
-                );
-
-                document.title = newDocument.title;
-
-                document.body.replaceWith(
-                    newDocument.body
-                );
-
-                document.body.className = "search-page";
-
-                window.history.pushState(
-                    { ourSearch: true },
-                    "",
-                    "/search.html"
-                );
-
-                window.scrollTo(0, 0);
-
-                var input =
-                    document.getElementById("search-input");
-
-                if (input) {
-                    setTimeout(function () {
-                        input.focus();
-                    }, 50);
-                }
-            })
-            .catch(function (error) {
-                console.error(
-                    "Our Search navigation error:",
-                    error
-                );
-            });
-    }
-
     document.addEventListener(
         "click",
         function (event) {
@@ -66,7 +14,7 @@
             event.preventDefault();
             event.stopPropagation();
 
-            openSearchPage();
+            window.location.href = "/search.html";
         },
         true
     );
